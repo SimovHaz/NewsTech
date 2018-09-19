0f81a9408607c7f5402e4486f9acf4360e67654a
SimovHaz <simouca95@gmail.com>
2018-09-19 09:31:31 +0100

    Update AppDelegate.swift

--- NewsTech/AppDelegate.swift
+++ NewsTech/AppDelegate.swift
@@ -18,8 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         let configuration = ParseClientConfiguration {
-            $0.applicationId = "FA55638B3F62A6FB2C6A76264D438"
-            $0.server = "https://test.parse-server.karizma1.com/parse/"
+            $0.applicationId = "***************"
+            $0.server = "*********************"
         }
         Parse.initialize(with: configuration)
         IQKeyboardManager.sharedManager().enable = true
